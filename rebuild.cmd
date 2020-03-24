4010053942ae7e0aa5851e36c4aa893d226026f0
xsthunder <xsthunder@outlook.com>
2020-03-24 19:04:02 +0800

    fire pip

--- rebuild.cmd
+++
@@ -1,3 +0,0 @@
-pip uninstall xsthunder-python-lib -y
-python setup.py sdist bdist_wheel
-pip install .\dist\xsthunder_python_lib-0.0.1-py3-none-any.whl
--- setup.py
+++ setup.py
@@ -1,18 +1,23 @@
 import setuptools
 
-with open("README.md", "r", encoding="UTF-8") as fh:
-    long_description = fh.read()
+def read_file(name):
+    with open(name, "r", encoding="UTF-8") as f:
+        return f.read()
+
+version = read_file("./config/version.txt")
+project_name = read_file("./config/project_name.txt")
+long_description = read_file('./README.md')
     
 
 setuptools.setup(
-    name="xsthunder-python-lib", # Replace with your own username
-    version="0.0.1",
+    name=project_name,
+    version=version,
     author="xsthunder",
     author_email="xsthunder@outlook.com",
     description="personal lib",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/xsthunder/xsthunder-python-lib",
+    url=f"https://github.com/xsthunder/{project_name}",
     packages=setuptools.find_packages(),
     classifiers=[
         "Programming Language :: Python :: 3",
@@ -22,4 +27,3 @@ setuptools.setup(
     install_requires=['IPython'],
     python_requires='>=3.4',
 )
-
