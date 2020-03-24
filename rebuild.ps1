119723bcb953ecc0700ec05a90209c744b41c2ea
xsthunder <xsthunder@outlook.com>
2020-03-24 22:30:32 +0800

    add deps

--- rebuild.ps1
+++ rebuild.ps1
@@ -4,7 +4,7 @@ $ErrorActionPreference = "Stop" # exit on error
 $version=get-content ./config/version.txt
 $project_name=get-content ./config/project_name.txt
 
-# pip install setuptools wheel # --user
+# pip install setuptools wheel twine # --user
 
 pip uninstall $project_name -y
 python setup.py sdist bdist_wheel
