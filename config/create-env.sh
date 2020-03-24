0c9dd6cd2572645da9dc6adec75cb0441e959434
xsthunder <xsthunder@outlook.com>
2020-03-24 18:14:38 +0800

    Update create-env.sh

--- config/create-env.sh
+++ config/create-env.sh
@@ -4,10 +4,14 @@ then
     source $HOME/miniconda/etc/profile.d/conda.sh
 fi
 
-# py3.7 for asyncio.WindowsProactorEventLoopPolicy() support
-conda create -n test IPython tqdm -c conda-forge -y
+env_name=test
+
+conda create -n $env_name IPython tqdm -c conda-forge -y
+
+conda activate $env_name
 yes | pip install sure
 
 # sure=1.4.11
 # ipython=7.8.0
 # tqdm=4.36.1
+# python=3.7.4
