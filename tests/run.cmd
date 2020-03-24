db1d54796df4c4449f8c865e203bd387272b3c25
xsthunder <xsthunder@outlook.com>
2020-03-24 16:53:43 +0800

    update run

--- tests/run.cmd
+++ tests/run.cmd
@@ -7,9 +7,10 @@ REM C:\Users\xiaos\Anaconda3\envs\py3torch-cpu\python.exe test_run_all_tests.py
 
 :: https://stackoverflow.com/questions/138497/iterate-all-files-in-a-directory-using-a-for-loop
 ::https://stackoverflow.com/questions/734598/how-do-i-make-a-batch-file-terminate-upon-encountering-an-error
-./test.sh
 
-set TRAVIS=TRUE
+
+REM set TRAVIS=true
+
 setlocal enabledelayedexpansion
 for %%f in (.\*.py) do (
   echo "-------------------------------------------------------------------------------------" "checking: %%f"
--- tests/run.sh
+++ tests/run.sh
@@ -3,13 +3,14 @@ set -e
 
 # set up conda env
 # https://docs.travis-ci.com/user/environment-variables/#default-environment-variables
-# TRAVIS=TRUE
+
 if test $TRAVIS
 then
     source $HOME/miniconda/etc/profile.d/conda.sh
     conda activate test
 fi
 
+# TRAVIS=true
 
 # run test
 for file in ./*.py
